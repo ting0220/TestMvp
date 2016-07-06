acc0639674e42b0bd7bfd3c5d20592eceac3b9b3
zhaoting <466490327@qq.com>
2016-07-06 17:50:12 +0800

    okhttp的更换

---
+++ app/src/main/java/com/example/z_ting/testmvp/NoConnected.java
@@ -0,0 +1,8 @@
+package com.example.z_ting.testmvp;
+
+/**
+ * Created by zhaoting on 16/7/6.
+ */
+public interface NoConnected {
+    void onNoConnected();
+}
---
+++ app/src/main/java/com/example/z_ting/testmvp/okhttpUtil/OkHttpUtil.java
@@ -0,0 +1,40 @@
+package com.example.z_ting.testmvp.okhttpUtil;
+
+import com.example.z_ting.testmvp.NoConnected;
+import com.example.zhaoting.utils.NetUtils;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+
+
+/**
+ * Created by zhaoting on 16/7/6.
+ */
+public class OkHttpUtil {
+    private NoConnected mNoConnected;
+    private OkHttpClient mOkHttpClient;
+
+
+    /**
+     * 使用内部类方式实现单例模式
+     */
+    private static class SingletonHolder {
+        private static OkHttpUtil instance = new OkHttpUtil();
+    }
+
+    private OkHttpUtil() {
+        mOkHttpClient = new OkHttpClient();
+    }
+
+    public static OkHttpUtil getInstance() {
+        return SingletonHolder.instance;
+    }
+
+    public void get(String url, Callback responseCallback, NoConnected mNoConnected) {
+        if (NetUtils.getInstance().isNetConnected()) {
+         
+
+        }
+    }
+
+}
