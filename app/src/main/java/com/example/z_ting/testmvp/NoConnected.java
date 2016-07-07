7f552c4190587cad7dc89866b3e0e88f38df2bd9
zhaoting <466490327@qq.com>
2016-07-07 16:41:42 +0800

    删除没有网络连接的接口

--- app/src/main/java/com/example/z_ting/testmvp/NoConnected.java
+++
@@ -1,8 +0,0 @@
-package com.example.z_ting.testmvp;
-
-/**
- * Created by zhaoting on 16/7/6.
- */
-public interface NoConnected {
-    void onNoConnected();
-}
--- app/src/main/java/com/example/z_ting/testmvp/app/BaseView.java
+++ app/src/main/java/com/example/z_ting/testmvp/app/BaseView.java
@@ -1,11 +1,9 @@
 package com.example.z_ting.testmvp.app;
 
-import com.example.z_ting.testmvp.NoConnected;
-
 /**
  * Created by zhaoting on 16/7/6.
  */
-public interface BaseView<T> extends NoConnected{
+public interface BaseView<T> {
     void setPresenter(T presenter);
 
     void onError();
