4b3870026593194f4571d8dd92f46148c91fdb8b
zhaoting <466490327@qq.com>
2016-07-11 17:36:03 +0800

    更改OKHttpUtil

--- app/src/main/java/com/example/z_ting/testmvp/contract/MainContract.java
+++ app/src/main/java/com/example/z_ting/testmvp/contract/MainContract.java
@@ -1,9 +1,16 @@
-Unstaged changes after reset:
-M	app/build.gradle
-D	app/src/main/java/com/example/z_ting/testmvp/MainView/MainContract.java
-M	app/src/main/java/com/example/z_ting/testmvp/app/BaseView.java
-M	app/src/main/java/com/example/z_ting/testmvp/contract/MainContract.java
-M	app/src/main/java/com/example/z_ting/testmvp/model/StoryModelImpl.java
-M	app/src/main/java/com/example/z_ting/testmvp/presenter/MainPresenter.java
-M	app/src/main/java/com/example/z_ting/testmvp/view/MainActivity.java
-M	utils/build.gradle
+package com.example.z_ting.testmvp.contract;
+
+import com.example.z_ting.testmvp.app.BasePresenter;
+import com.example.z_ting.testmvp.app.BaseView;
+
+public interface MainContract {
+    interface MainView extends BaseView<MainPresenter> {
+
+    }
+
+
+    interface MainPresenter extends BasePresenter {
+
+    }
+
+}
